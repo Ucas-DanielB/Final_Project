e2d9c09c7d084adcbf239aca4110321b94472f4b
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-05-05 10:16:02 -0600

    main thing

--- code_tasks/tasks_main.py
+++ code_tasks/tasks_main.py
@@ -15,8 +15,27 @@
 
 def managing_tasks():
 
-    pass
-
+    while True:
+        try:    
+            main_tasks = int(input("""Welcome to your Task Manager, what would you like to do?
+                           1. Make Tasks.
+                           2. Delete Tasks.
+                           3. Mark down Tasks.
+                           4. Return back.
+                           5. Exit program.\n"""))
+            if main_tasks == 1:
+                pass
+            elif main_tasks == 2:
+                pass
+            elif main_tasks == 3:
+                pass
+            elif main_tasks == 4:
+                pass
+            elif main_tasks == 5:
+                print("Okay, goodbye!")
+                break
+        except:
+            print("This doesn't work for whatever reason! Try using a correct response.")
 
 
 
