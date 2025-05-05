514060136d99f6ed4e875057d1b6aa59459939ac
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-05-05 10:32:05 -0600

    csv stuff

--- code_tasks/tasks_main.py
+++ code_tasks/tasks_main.py
@@ -12,6 +12,20 @@
 ####
 ####
 
+import csv
+
+#Define file path, to_do_list.py\to_do_list.txt
+folder = "tasks"
+file_name = "tasks.csv"
+file_path = csv.path.join(folder, file_name)
+
+#Ensure the folder and file exist
+if not csv.path.exists(folder):
+    csv.makedirs(folder)
+
+if not csv.path.exists(file_path):
+    with open(file_path, "w") as f:
+        pass  #Create an empty file
 
 def managing_tasks():
 
@@ -21,18 +35,22 @@ def managing_tasks():
                            1. Make Tasks.
                            2. Delete Tasks.
                            3. Mark down Tasks.
-                           4. Return back.
-                           5. Exit program.\n"""))
+                           4. View all Tasks.
+                           5. Return back.
+                           6. Exit program.\n"""))
             if main_tasks == 1:
-                pass
+                print("Welcome! What would you like to Add?")
             elif main_tasks == 2:
-                pass
+                print("Welcome! What would you like to Remove?")
             elif main_tasks == 3:
-                pass
+                print("Welcome! What Tasks would you like to Mark down?")
             elif main_tasks == 4:
+                print("Welcome! Here are all your tasks!")
+                
+            elif main_tasks == 5:
                 print("Okay, see you later!")
                 return
-            elif main_tasks == 5:
+            elif main_tasks == 6:
                 print("Okay, goodbye!")
                 break
         except ValueError:
